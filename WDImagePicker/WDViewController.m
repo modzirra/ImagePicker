67cbcc0b77b160a12e049c444e30b84f0b8f6ea7
Modzirra <spampiston@gmail.com>
2013-09-18 15:48:49 -0700

    Added Nav Bar
    Fixed Storyboard, so much better!
    To do:  Wire up camera, add delete functionality, add nav buttons

--- WDImagePicker/WDViewController.m
+++ WDImagePicker/WDViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "WDViewController.h"
+//#import "CFCameraViewController.h"
 
-@interface WDViewController ()
+@interface WDViewController ()<UIImagePickerControllerDelegate>{
+    __weak IBOutlet UIImageView *imageView;
+}
 
 @end
 
@@ -29,6 +32,43 @@
 - (IBAction)startImagePicker:(id)sender{
     UIImagePickerController *picker = [UIImagePickerController new];
     [self presentViewController:picker animated:YES completion:nil];
+    picker.delegate = self;
+    picker.allowsEditing = YES;
+    if ([UIImagePickerController isSourceTypeAvailable:(UIImagePickerControllerSourceTypeCamera)]) {
+        [picker setSourceType:(UIImagePickerControllerSourceTypeCamera)];
+    }
+    //check for a camera on the device and set it if available
+     [self presentViewController:picker animated:YES completion:nil];
     // create image picker and implement
 }
+
+- (void)imagePickerController:
+(UIImagePickerController *) picker didFinishPickingMediaWithInfo:
+(NSDictionary *)info{
+    UIImage * newImage = info[UIImagePickerControllerOriginalImage];
+    [self dismissViewControllerAnimated: YES completion:^{
+        imageView.image = new image;
+    }];
+}
+//select image
+
+
+-(void) prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    if( [segue.destinationViewController isKindOfClass:[CFCameraViewController class]] ) {
+        ((CFCameraViewController*)segue.destinationViewController).delegate = self;
+    }
+}
+
+
+#pragma mark - Camera delegate
+
+-(void) cameraViewController:(CFCameraViewController*)cameraVC
+             didCaptureImage:(UIImage*)image
+{
+    imageView.image = image;
+    [self.navigationController popToViewController:self animated:YES];
+}
+
+
 @end
